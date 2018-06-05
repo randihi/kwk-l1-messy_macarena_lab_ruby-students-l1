08c4433d5a76de1ea7a447396f9bbf7a913a0075
randihi <randihigashi@gmail.com>
2018-06-05 17:46:55 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,16 @@
+def about_me
+  puts "My name is Randi"
+  puts "I was born in California"
+  puts "I am 17 years old"
+  puts "I love to run track"
+end
+
+def part_two
+  puts "I have two sisters"
+  puts "I am the middle child"
+  puts "I have a dog"
+  puts "I live in Colorado"
+end
+
+about_me
+part_two
